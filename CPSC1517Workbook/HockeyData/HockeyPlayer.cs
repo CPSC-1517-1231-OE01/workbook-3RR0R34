47b55de9fa51a953f3a7eccad0b0354fce90eae8
3RR0R34 <127166244+3RR0R34@users.noreply.github.com>
2023-09-07 21:21:44 -0600

    Created stuff
    Start of HockeyPlayer Class
    created position enum
    created shots enum

---
+++ CPSC1517Workbook/HockeyData/HockeyPlayer.cs
@@ -0,0 +1,58 @@
+﻿namespace Hockey.Data
+{
+    public class HockeyPlayer
+    {
+        //data fields
+        private string _firstName;
+        private string _lastName;
+        private string _BirthPlace;
+        private DateOnly _DateofBirth;
+        private int _heightinInches;
+        private int _weightinPounds;
+
+        private Position _position;
+        private Shots _shots;
+        //properties
+        public String FirstName 
+        {
+            get;
+            set;
+        }
+        public String LastName
+        {
+            get;
+            set;
+        }
+        public String BirthPlace
+        {
+            get;
+            set;
+        }
+        public DateOnly DateofBirth
+        {
+            get;
+            set;
+        }
+        public int HeightinInches
+        {
+            get;
+            set;
+        }
+        public int WeightinPounds 
+        {
+            get;
+            set;
+        }
+        public Position Position
+        {
+            get;
+            set;
+        }
+        public Shots Shots
+        {
+            get;
+            set;
+        }
+        //constructors
+    }
+}
---
+++ CPSC1517Workbook/HockeyData/Position.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hockey.Data
+{
+    /// <summary>
+    /// Represents the available hockey player positions
+    /// </summary>
+    public enum Position
+    {
+        LeftWing, //0
+        RightWing, //1
+        Center, //2
+        Defense, //3
+        Goalie //4
+    }
+}
